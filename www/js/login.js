da1c689867f871422762ba31e473107b16a7f1ad
Sebastian <lalorossi96@gmail.com>
2018-06-06 12:55:56 -0300

    Primer version de checkeo de usuarios

--- www/js/login.js
+++ www/js/login.js
@@ -1,1 +1,21 @@
-index.js
+//Valida usuario y contraseña del formulario con los datos esperados de usuario y contraseña
+function validarLogin(formulario) {
+	//Accede a los datos ingresados
+	var user = document.getElementById("inpUser").value;
+	var pass = document.getElementById("inpPass").value;
+
+	//Aca habria que usar los objetos de persona para obtener la contraseña encriptada
+	//Y comparar con una encriptacion del ingreso
+	if(user =="guady" && pass == "jaima"){
+		return true;
+	}
+	if(user =="juli" && pass == "salem"){
+		return true;
+	}
+	else{
+		//Si los datos ingresados no coinciden con los esperados, no hace submit
+		window.alert("Usuario o contraseña no validos");
+		return false;
+	}
+		
+}
